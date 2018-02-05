2cc761a090f410038df9aba43f3293aa29ea83dd
Tanis Webb <taniswebb@gmail.com>
2018-02-05 23:14:18 +0100

    testing vue

---
+++ vue.js
@@ -0,0 +1,19 @@
+const app = new Vue({
+  el: '#app',
+  data: {
+    navbar: {
+      buy: {
+        string: "Buy",
+        link: "buy.html"
+      },
+      trial: {
+        string: "Trial",
+        link: "trial.html"
+      },
+      forum: {
+        string: "Forum",
+        link: "forum/"
+      },
+    }
+  }
+})
